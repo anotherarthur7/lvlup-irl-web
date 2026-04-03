d0803e935797df3dd93f521ffa164335c6b41750
another_arthur <arturgaynanov7@gmail.com>
2026-04-03 19:47:05 +0700

    update1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // НЕ используй output: 'export' если нужны API маршруты
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
