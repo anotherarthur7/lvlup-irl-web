517222cd4dbaa48cda40220337e8bf2dfecd96a7
another_arthur <arturgaynanov7@gmail.com>
2026-04-21 20:59:53 +0700

    supabaseTest3

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-if (!supabaseUrl || !supanonKey) {
+if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('supabaseUrl and supabaseAnonKey are required. Check your environment variables.')
 }
 
